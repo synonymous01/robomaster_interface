fixed angle issues (converted them to radians now)

src/pub_data.py
@@ -66,14 +66,14 @@ class rover:
 
     def update_imu(self, imu_info):
         acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z = imu_info
-        acc_z=9.8
+        # acc_z= 9.8
         sending = Imu()
         sending.header.frame_id = '{}_imu'.format(self.robot_name)
         sending.header.stamp = rospy.Time.now()
 
 
-        gyro_x = 0
-        gyro_y = 0
+        gyro_x = gyro_x * (np.pi / 180)
+        gyro_y = gyro_y * (np.pi / 180)
         gyro_z = gyro_z * (np.pi/180)
         sending.linear_acceleration.x = acc_x
         sending.linear_acceleration.y = acc_y
@@ -164,7 +164,10 @@ class rover:
 
 
     def update_angles(self, angle_info):
-        self.yaw, _, _ = angle_info
+        y, p, r = angle_info
+        self.yaw = y * (np.pi/ 180)
+        self.pitch = p * (np.pi/180)
+        self.roll = r * (np.pi/180)
 
     def send_velocities(self, data):
         v = data.linear.x