5e1b2b3918795299d8ea4467bebef90be56f1784
Ibrahim <ibrahimsa01@hotmail.com>
2024-01-31 20:29:55 +0500

    updated test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import os
 
 print('file name: {}'.format(os.path.basename(__file__)))
-print('directory name: {}'.format(os.path.basename(__file__)))
+print('directory name: {}'.format(os.path.dirname(__file__)))
